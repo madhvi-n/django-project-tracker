f9efdfd0269196532320b6fd98a4ce4de4b0f2ed
madhvi-n <madhvinandanwar@gmail.com>
2023-03-06 13:39:15 +0530

    commit: readme

--- apps/boards/serializers/board_section.py
+++ apps/boards/serializers/board_section.py
@@ -4,9 +4,16 @@ from boards.models import BoardSection
 
 
 class BoardSectionSerializer(serializers.ModelSerializer):
+    tasks = serializers.SerializerMethodField()
+
     class Meta:
         model = BoardSection
-        fields = ('id', 'title', 'board', 'max_issues_limit')
+        fields = ('id', 'title', 'board', 'max_issues_limit', 'tasks')
 
     def create(self, validated_data):
         return BoardSection.objects.create(**validated_data)
+
+    def get_tasks(self, obj):
+        if hasattr(obj, 'tasks'):
+            return obj.tasks.count()
+        return 0
